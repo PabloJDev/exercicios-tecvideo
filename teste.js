5c1669c2dbf36b36049669725932c83c49e6d169
Pablo JDev <pablodosantos2007@gmail.com>
2025-08-21 12:08:31 -0300

    exercicio 1

---
+++ teste.js
@@ -0,0 +1,12 @@
+//soma: 2 + 4 = x
+//Subtração: 2 - 4 = x
+//Divisão: 2 / 4 = x
+//Multiplicação: 2 * 4 = x
+
+let num1 = 2;
+let num2 = 4;
+
+console.log ("Soma: 2 + 4 =", num1 + num2)
+console.log ("Subtração: 2 - 4 =", num2 - num1)
+console.log ("Divisão: 2 / 4 =", num1 / num2)
+console.log ("Multiplcação: 2 * 4 =", num1 * num2)
